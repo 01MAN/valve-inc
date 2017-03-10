a95d9ba009ae42dc038644e3cd5e36d2c4c917d9
Ольман Роман Юрьевич <iv622s12@lwp02.csc.local>
2017-03-10 14:28:25 +0700

    all

--- trpo228.c
+++ trpo228.c
@@ -1,3 +1,36 @@
+#include <stdio.h>
+int vklad(int q, int w){
+    int e;
+    if (q < 1000000){
+        if (w < 31){
+            e = q - q * 0.1;
+        };
+        if ((w > 30) && (w < 121)){
+            e = q + q * 0.02;
+        };
+        if ((w > 120) && (w < 241)){
+            e = q + q * 0.06;
+        };
+        if ((w > 240) && (w < 366)){
+            e= q + q * 0.12;
+        };
+    } 
+    else{
+        if (w < 31){
+            e = q - q * 0.1;
+        };
+        if ((w > 30) && (w < 121)){
+            e = q + q * 0.03;
+        };
+        if ((w > 120) && (w < 241)){
+            e = q + q * 0.08;
+        };
+        if ((w > 240) && (w < 366)){
+            e= q + q * 0.15;
+        };
+    };
+    return e;
+};        
 int main(){
     int sum, data, vihod;
     printf("Sum vklada: ");
